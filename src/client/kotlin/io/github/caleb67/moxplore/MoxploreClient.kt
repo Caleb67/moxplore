f7a6e25773b70de74e0b3893b0e263d96817c3be
Caleb67 <57026376+Caleb67@users.noreply.github.com>
2024-03-08 16:40:05 -0800

    Add files via upload

---
+++ src/client/kotlin/io/github/caleb67/moxplore/MoxploreClient.kt
@@ -0,0 +1,9 @@
+package io.github.caleb67.moxplore
+
+import net.fabricmc.api.ClientModInitializer
+
+object MoxploreClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/io/github/caleb67/moxplore/MoxploreDataGenerator.kt
@@ -0,0 +1,9 @@
+package io.github.caleb67.moxplore
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object MoxploreDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
