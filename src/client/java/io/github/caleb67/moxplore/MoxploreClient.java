56d527ce1f85a661bd1c4285cd1a614dcda56193
BuildTools <unconfigured@null.spigotmc.org>
2024-03-12 19:12:37 -0700

    new file:   .gitattributes 	new file:   .github/workflows/build.yml 	new file:   .gitignore 	new file:   LICENSE 	new file:   build.gradle 	new file:   gradle.properties 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   settings.gradle 	new file:   src/client/java/io/github/caleb67/moxplore/MoxploreClient.java 	new file:   src/client/java/io/github/caleb67/moxplore/mixin/client/ExampleClientMixin.java 	new file:   src/client/resources/moxplore.client.mixins.json 	new file:   src/main/java/io/github/caleb67/moxplore/Moxplore.java 	new file:   src/main/java/io/github/caleb67/moxplore/mixin/ExampleMixin.java 	new file:   src/main/resources/assets/moxplore/icon.png 	new file:   src/main/resources/fabric.mod.json 	new file:   src/main/resources/moxplore.mixins.json

---
+++ src/client/java/io/github/caleb67/moxplore/MoxploreClient.java
@@ -0,0 +1,10 @@
+package io.github.caleb67.moxplore;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoxploreClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
