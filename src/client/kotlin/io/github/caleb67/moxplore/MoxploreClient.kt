f9e1a1c0db3be8da92aff7ad37ca4c9fcaf39844
Caleb67 <57026376+Caleb67@users.noreply.github.com>
2024-03-12 19:15:11 -0700

    Delete src directory

--- src/client/kotlin/io/github/caleb67/moxplore/MoxploreClient.kt
+++
@@ -1,9 +0,0 @@
-package io.github.caleb67.moxplore
-
-import net.fabricmc.api.ClientModInitializer
-
-object MoxploreClient : ClientModInitializer {
-	override fun onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
-}
--- src/main/kotlin/io/github/caleb67/moxplore/MoxploreDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package io.github.caleb67.moxplore
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object MoxploreDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
